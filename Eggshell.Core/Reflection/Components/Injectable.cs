4942e9dd676b0d8a788def9c9d1c5fb66a575944
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-05-27 16:38:17 +1000

    Create Injectable.cs

---
+++ Eggshell.Core/Reflection/Components/Injectable.cs
@@ -0,0 +1,13 @@
+﻿using Eggshell.Reflection;
+
+namespace Eggshell
+{
+	/// <summary>
+	/// Allows a member to have injected components into it. Basically
+	/// builds the source tree to allow custom logic to be injected
+	/// </summary>
+	public class Injectable : IBinding
+	{
+		public void OnAttached( Library item ) { }
+	}
+}
