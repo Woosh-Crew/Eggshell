fdb6a661b4dc8a6fe39aae12a39adc04d7982c41
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-05-29 17:31:43 +1000

    Add Coroutine system

---
+++ Eggshell.Core/Coroutine/Coroutine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Eggshell
+{
+	public class Wait : IEnumerator
+	{
+		public object Current { get; }
+
+		public bool MoveNext()
+		{
+			return false;
+		}
+
+		public void Reset() { }
+	}
+
+	public class Coroutine : Module
+	{
+		// Coroutine API
+		// --------------------------------------------------------------------------------------- //
+
+		public static void Start( IEnumerator enumerator )
+		{
+			enumerator.MoveNext();
+
+			Get<Coroutine>().Running.Add( enumerator );
+		}
+
+		public static void Start( Func<IEnumerator> func )
+		{
+			var enumerator = func.Invoke();
+			enumerator.MoveNext();
+
+			Get<Coroutine>().Running.Add( enumerator );
+		}
+
+		// Internal Module
+		// --------------------------------------------------------------------------------------- //
+
+		private List<IEnumerator> Running { get; } = new();
+
+		public override void OnUpdate()
+		{
+			base.OnUpdate();
+
+			for ( var i = Running.Count; i > 0; i-- )
+			{
+				var remove = Running[i - 1].MoveNext();
+
+				if ( remove )
+				{
+					Running.RemoveAt( i - 1 );
+				}
+			}
+		}
+	}
+}
--- Eggshell.Tests/Program.cs
+++ Eggshell.Tests/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Eggshell.Tests
+﻿using System.Collections;
+
+namespace Eggshell.Tests
 {
 	[Archive( Fallback = "game://testing.txt" )]
 	public class Testing : ILibrary
@@ -17,6 +19,29 @@
 		public static void Main( string[] args )
 		{
 			Crack( new() );
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+			Bootstrap.Update();
+		}
+
+		public override void OnReady()
+		{
+			Terminal.Log.Info( "Hello World" );
+			Coroutine.Start( Action() );
+		}
+
+		public IEnumerator Action()
+		{
+			Terminal.Log.Info( "First" );
+
+			yield return null;
+
+			Terminal.Log.Info( "Second" );
 		}
 	}
 }
