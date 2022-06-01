7618da3c4f944c15c2544c8238311da66ec0e7ad
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-06-01 21:51:27 +1000

    Add Stuff from operations class right into Routines static api

--- Eggshell.Core/Modules/Routine/Routines.cs
+++ Eggshell.Core/Modules/Routine/Routines.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Eggshell
 {
@@ -109,11 +110,32 @@ namespace Eggshell
 		}
 
 		/// <summary>
+		/// Creates a IRoutine based on an Action that has an Action as one of its parameters 
+		/// (I know its weird) can be quite useful in a handful of scenarios (Such as prototyping
+		/// a routine) 
+		/// </summary>
+		public static IRoutine Create( Action<Action> loaded, string text )
+		{
+			// Really weird I know...
+			return new ActionBasedCallback( loaded, text );
+		}
+
+		/// <summary>
+		/// Creates a IRoutine based on a task. The finish callback will be invoked once this task 
+		/// has been completed. Progress depends on the task type.
+		/// </summary>
+		public static IRoutine Create( Task loaded, string text )
+		{
+			var operation = new TaskBasedCallback( loaded, text );
+			return operation;
+		}
+
+		/// <summary>
 		/// The time it took for the last routine that was running to completely finish. This
 		/// is mostly used as debugging information.
 		/// </summary>
 		public Stopwatch Timing { get; private set; }
-		
+
 		private Action Finished { get; set; }
 		private IRoutine Current { get; set; }
 
@@ -230,6 +252,9 @@ namespace Eggshell
 			Load();
 		}
 
+		// Internal Classes
+		// --------------------------------------------------------------------------------------- //
+		
 		private class Request
 		{
 			public bool Injected { get; internal set; }
@@ -263,6 +288,58 @@ namespace Eggshell
 			{
 				_cached = request;
 				Injected = false;
+			}
+		}
+
+		private class ActionBasedCallback : IRoutine
+		{
+			private readonly Action<Action> _operation;
+
+			public ActionBasedCallback( Action<Action> loaded, string text )
+			{
+				Text = text;
+				_operation = loaded;
+			}
+
+			// Loadable
+
+			public float Progress { get; }
+			public string Text { get; }
+
+			public void Load( Action loaded )
+			{
+				_operation.Invoke( loaded );
+			}
+		}
+
+		private class TaskBasedCallback : IRoutine
+		{
+			private readonly Task _task;
+
+			public TaskBasedCallback( Task loaded, string text )
+			{
+				Text = text;
+				_task = loaded;
+			}
+
+			// Loadable
+
+			public float Progress { get; set; }
+			public string Text { get; }
+
+			public async void Load( Action loaded )
+			{
+				try
+				{
+					// Not sure if this is a good idea?
+					await _task;
+				}
+				catch ( Exception e )
+				{
+					Terminal.Log.Exception( e );
+				}
+
+				loaded.Invoke();
 			}
 		}
 	}
